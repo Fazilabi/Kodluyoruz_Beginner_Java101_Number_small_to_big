e8010177d03bce803cec8d0012e8bb8200078ae1
Fazilabi <fazilag@code.edu.az>
2022-11-26 12:52:12 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2, num3;
+        System.out.print("Enter num1 :");
+        num1 = input.nextInt();
+        System.out.print("Enter num2 :");
+        num2 = input.nextInt();
+        System.out.print("Enter num3 :");
+        num3 = input.nextInt();
+        if ((num1 < num2) && (num1 < num3)) {
+
+            if (num2 < num3) {
+                System.out.println(num1 + " < " + num2 + " < " + num3);
+            } else {
+                System.out.println(num1 + " < " + num3 + " < " + num2);
+            }
+
+        } else if ((num2 < num1) && (num2 < num3)) {
+            if (num1 < num3) {
+                System.out.println(num2 + " < " + num1 + " < " + num3);
+            } else {
+                System.out.println(num2 + " < " + num3 + " < " + num1);
+            }
+        } else if((num3<num1) && (num3<num2)) {
+            if (num1<num2){
+                System.out.println(num3 + " < " + num1 + " < " + num2);
+
+            }else {
+                System.out.println(num3 + " < " + num2 + " < " +num1);
+            }
+        } else if (num1==num2 && num1==num3) {
+            System.out.println("All numbers equal");
+        } else if(num1==num2 && num1<num3){
+            System.out.println(num1 + " = " + num2 + " < " +num3);
+        } else if (num2==num3 && num2<num1) {
+            System.out.println(num2 + " = " + num3 + " < " + num1);
+        }
+
+    }
+}
